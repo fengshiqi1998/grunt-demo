eaf0668c89cfbd343df2002223c092bd89ab71d5
fengshiqi1998 <512653808@qq.com>
2019-03-05 16:15:35 +0800

    eslint

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    eslint: {
+      options: {
+        configFile: '.eslintrc.json'
+      },
+      target: ['rectangle.js']
+    }
+  });
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.registerTask('default', ['eslint']);
+};
+
